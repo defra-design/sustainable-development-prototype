85c3df701ead8a104077e84a6ba2b80dd93df082
Dave Haigh <davehaigh@outlook.com>
2021-06-23 09:29:11 +0100

    initial setup

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,11 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Base session data
+var _myData = {
+    "includeValidation": "true"
+}
+
+require('./routes/1-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
+
 module.exports = router
---
+++ app/routes/1-0/routes.js
@@ -0,0 +1,46 @@
+module.exports = function (router,_myData) {
+
+    var version = "1-0";
+
+    function reset(req){
+        req.session.myData = JSON.parse(JSON.stringify(_myData))
+
+        // Default setup
+        req.session.myData.service = "apply"
+
+        //Default answers
+
+    }
+
+    // Every GET and POST
+    router.all('/' + version + '/*', function (req, res, next) {
+        if(!req.session.myData || req.query.r) {
+            reset(req)
+        }
+
+        //version
+        req.session.myData.version = version
+
+        // Reset page validation to false by default. Will only be set to true, if applicable, on a POST of a page
+        req.session.myData.validationErrors = {}
+        req.session.myData.validationError = "false"
+        req.session.myData.includeValidation =  req.query.includeValidation || req.session.myData.includeValidation
+
+        //Reset page notifications
+        req.session.myData.notifications = {}
+        req.session.myData.showNotification = "false"
+
+        //defaults for setup
+        // req.session.myData.example =  req.query.eg || req.session.myData.example
+
+        next()
+    });
+
+    // Prototype setup
+    router.get('/' + version + '/setup', function (req, res) {
+        res.render(version + '/setup', {
+            myData:req.session.myData
+        });
+    });
+
+}
