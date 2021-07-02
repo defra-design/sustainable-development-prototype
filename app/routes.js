9385c0afa4a8719325a86776046146f4e387d89f
Dave Haigh <davehaigh@outlook.com>
2021-07-02 16:23:33 +0100

    1-0 bats list

--- app/routes.js
+++ app/routes.js
@@ -5,8 +5,88 @@ const router = express.Router()
 
 // Base session data
 var _myData = {
-    "includeValidation": "true"
+    "includeValidation": "true",
+    "batSpecies": [
+        {
+            "id": "_bat-1",
+            "name": "Alcathoe"
+        },
+        {
+            "id": "_bat-2",
+            "name": "Nathusius’ pipistrelle"
+        },
+        {
+            "id": "_bat-3",
+            "name": "Barbastelle"
+        },
+        {
+            "id": "_bat-4",
+            "name": "Natterer’s"
+        },
+        {
+            "id": "_bat-5",
+            "name": "Bechstein’s"
+        },
+        {
+            "id": "_bat-6",
+            "name": "Greater horseshoe"
+        },
+        {
+            "id": "_bat-7",
+            "name": "Noctule"
+        },
+        {
+            "id": "_bat-8",
+            "name": "Brandt’s"
+        },
+        {
+            "id": "_bat-9",
+            "name": "Serotine"
+        },
+        {
+            "id": "_bat-10",
+            "name": "Brown long-eared"
+        },
+        {
+            "id": "_bat-11",
+            "name": "Grey long-eared"
+        },
+        {
+            "id": "_bat-12",
+            "name": "Soprano pipistrelle"
+        },
+        {
+            "id": "_bat-13",
+            "name": "Common pipistrelle"
+        },
+        {
+            "id": "_bat-14",
+            "name": "Leisler’s"
+        },
+        {
+            "id": "_bat-15",
+            "name": "Whiskered"
+        },
+        {
+            "id": "_bat-16",
+            "name": "Daubenton’s"
+        },
+        {
+            "id": "_bat-17",
+            "name": "Lesser horseshoe"
+        }
+    ]
 }
+
+//Sort bats
+_myData.batSpecies.sort(function(a,b){
+    if (a.name.toUpperCase() < b.name.toUpperCase()){
+        return -1
+    } else if(a.name.toUpperCase() > b.name.toUpperCase()){
+        return 1
+    }
+    return 0;
+});
 
 require('./routes/1-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 
--- app/routes/1-0/routes.js
+++ app/routes/1-0/routes.js
@@ -2,6 +2,18 @@ module.exports = function (router,_myData) {
 
     var version = "1-0";
 
+    function setSelectedBat(req, _batParameter){
+        if(_batParameter){
+            for (var i = 0; i < req.session.myData.batSpecies.length; i++) {
+                var _thisBat = req.session.myData.batSpecies[i]
+                if(_thisBat.id.toString() == _batParameter.toString()){
+                    req.session.myData.selectedBat = _thisBat
+                    req.session.myData.bat = _thisBat.id
+                }
+            }
+        }
+    }
+
     function reset(req){
         req.session.myData = JSON.parse(JSON.stringify(_myData))
 
@@ -9,6 +21,7 @@ module.exports = function (router,_myData) {
         req.session.myData.service = "apply"
 
         //Default answers
+        req.session.myData.bat = "_bat-1"
 
     }
 
@@ -33,6 +46,10 @@ module.exports = function (router,_myData) {
         //defaults for setup
         // req.session.myData.example =  req.query.eg || req.session.myData.example
 
+        //Constant checks for query
+        req.session.myData.bat = req.query.bat || req.session.myData.bat
+        setSelectedBat(req,req.session.myData.bat)
+
         next()
     });
 
@@ -41,6 +58,73 @@ module.exports = function (router,_myData) {
         res.render(version + '/setup', {
             myData:req.session.myData
         });
+    });
+
+    // BAT Species
+    router.get('/' + version + '/species-bat', function (req, res) {
+        res.render(version + '/species-bat', {
+            myData:req.session.myData
+        });
+    });
+    router.post('/' + version + '/species-bat', function (req, res) {
+
+        req.session.myData.speciesBatAnswerTemp = req.body.bat
+        console.log(req.session.myData.speciesBatAnswerTemp)
+        if(req.session.myData.includeValidation == "false"){
+            req.session.myData.speciesBatAnswerTemp = req.session.myData.speciesBatAnswerTemp || "_bat-1"
+        }
+        if(req.session.myData.speciesBatAnswerTemp == "_unchecked"){
+            req.session.myData.validationError = "true"
+            req.session.myData.validationErrors.speciesBatAnswer = {
+                "anchor": "_bat-1",
+                "message": "Select the species of bat that will be affected"
+            }
+        }
+        
+        if(req.session.myData.validationError == "true") {
+            req.session.myData.selectedBatTotal = 0
+            req.session.myData.batSpecies.forEach(function(_bat, index) {
+                _bat.selected = false
+            });
+            res.render(version + '/species-bat', {
+                myData: req.session.myData
+            });
+        } else {
+            req.session.myData.speciesBatAnswer = req.session.myData.speciesBatAnswerTemp
+            req.session.myData.speciesBatAnswerTemp = ''
+
+            req.session.myData.selectedBats = []
+            req.session.myData.batSpecies.forEach(function(_bat, index) {
+                if(req.session.myData.speciesBatAnswer.indexOf(_bat.id.toString()) != -1){
+                    req.session.myData.selectedBats.push(_bat)
+                }
+            });
+            req.session.myData.bat = req.session.myData.selectedBats[0].id
+            req.session.myData.selectedBatLoop = 1
+            setSelectedBat(req,req.session.myData.bat)
+
+            res.redirect(301, '/' + version + '/activities-bat?bat=' + req.session.myData.selectedBat.id);
+        }
+    });
+
+    // BAT Activities
+    router.get('/' + version + '/activities-bat', function (req, res) {
+        res.render(version + '/activities-bat', {
+            myData:req.session.myData
+        });
+    });
+    router.post('/' + version + '/activities-bat', function (req, res) {
+
+        //loop check doesnt work back links
+        if(req.session.myData.selectedBatLoop < req.session.myData.selectedBats.length){
+            req.session.myData.bat = req.session.myData.selectedBats[req.session.myData.selectedBatLoop].id
+            setSelectedBat(req,req.session.myData.bat)
+            req.session.myData.selectedBatLoop++
+            res.redirect(301, '/' + version + '/activities-bat?bat=' + req.session.myData.selectedBat.id);
+        } else {
+            res.redirect(301, '/' + version + '/next-page');
+        }
+
     });
 
 }
