be2b27ab23d3ebeb1055fc0372faab174219fa8f
Dave Haigh <davehaigh@outlook.com>
2021-11-21 18:27:56 +0000

    updated config service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply for a licence to do work that may affect bats',
 
   // Default port that prototype runs on
   port: '3000',
