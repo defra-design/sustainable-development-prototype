2a1cb8d57d04fbdad46a8eef051e0dde0882c603
Stu Bamforth <STUART.BAMFORTH@dwp.gov.uk>
2022-10-19 14:09:40 +0100

    Added more pages to 585 flow

--- app/routes/private-beta/routes.js
+++ app/routes/private-beta/routes.js
@@ -259,6 +259,14 @@ router.post('/private-beta/SDDSIP-585-amend-permissions-flow/permissions-check',
   } 
 });
 
+router.post('/private-beta/SDDSIP-585-amend-permissions-flow/add-another-permission-check', (req, res) => {
+  if(req.session.data['add-another-permission-check'] == 'Yes'){
+      res.redirect('consent-type')
+  } else if(req.session.data['add-another-permission-check'] == 'No'){
+      res.redirect('redirect')
+  } 
+});
+
 
 
 
